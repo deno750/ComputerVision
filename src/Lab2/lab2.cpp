7892386e4d0634371185b279a3765c2d8a06fc43
deno750 <deronjic.denis@gmail.com>
2020-04-09 19:26:30 +0200

    continued lab2

--- src/Lab2/lab2.cpp
+++ src/Lab2/lab2.cpp
@@ -8,14 +8,13 @@
 
 using namespace std;
 int main() {
-    cout << "Hello world\n";
     DIR* dir;
     string path = "data/checkerboard_images/";
     dir = opendir(path.c_str());
     if (dir != NULL) {
-        cout << "Ottimo\n";
+        cout << "Direcotry found!\n";
     } else {
-        cout << "Non Ottimo\n";
+        cout << "Directory not found!\n";
     }
     struct dirent* image;
     
@@ -23,33 +22,55 @@ int main() {
     std::vector<std::vector<cv::Point2f>> imagePoints;
     cv::Mat cameraMatrix, distCoeffs, rvecs, tvecs;
     cv::Mat mat;
-    cv::Size chessSize(5, 6);
+    cv::Size chessSize(6, 5);
+    
+    std::vector<cv::Point3f> objc;
+    for (int y = 1; y <= chessSize.height; ++y) {
+        
+        for (int x = 1; x <= chessSize.width; ++x) {
+            objc.push_back(cv::Point3f(x * 0.11, y * 0.11, 0));
+        }
+    }
+    
+    for (int i = 0; i < objc.size(); ++i) {
+        cout << objc[i] << " ";
+    }
+    cout << endl;
+    
     while ((image = readdir(dir)) != NULL) {
         if (strcmp(image->d_name, ".") == 0 || strcmp(image->d_name, "..") == 0) {
             continue;
         }
+        
         string imagePath = path + image->d_name;
         mat = cv::imread(imagePath);
         if (mat.empty()) {
-            cout << "DIO CANE\n";
+            continue;
         }
+        
         std::vector<cv::Point2f> corners;
-        bool patternFound = cv::findChessboardCorners(mat, chessSize, corners); //This function works only with grey scale images. In this homeworks, images are already in grey. But if the images are not grey, we have to make the images in grey.
+        bool patternFound = cv::findChessboardCorners(mat, chessSize, corners); //This function works only with grey scale images. In this homework, images are already in grey.
         
         if (patternFound) {
             imagePoints.push_back(corners);
-            
-            //MARK: Not sure if it's correct. Leave it as it is for now
-            std::vector<cv::Point3f> objc;
-            for (int i = 0; i < corners.size(); ++i) {
-                cv::Point3f point = cv::Point3f(corners[i].x, corners[i].y, 0);
-                objc.push_back(point);
-            }
             objectPoints.push_back(objc);
             
         }
         
     }
-    cv::calibrateCamera(objectPoints, imagePoints, cv::Size(mat.cols, mat.rows), cameraMatrix, distCoeffs, rvecs, tvecs);
+    cv::calibrateCamera(objectPoints, imagePoints, mat.size(), cameraMatrix, distCoeffs, rvecs, tvecs);
+    cv::Mat R, map1, map2;
+    cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, R, cameraMatrix, mat.size(), CV_32FC1, map1, map2);
+    cv::Mat src, dst;
+    cout << "map1" << endl;
+    cout << map1 << endl;
+    src = cv::imread("data/test_image.png");
+    if (src.empty()) {
+        cout << "Image not found!\n";
+    }
+    cv::remap(src, dst, map1, map2, cv::INTER_CUBIC);
+    cv::imshow("ORIGINAL", src);
+    cv::imshow("REMAPPED", dst);
+    cv::waitKey(0);
     return 0;
 }
