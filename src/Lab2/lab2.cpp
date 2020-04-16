2a4ea96f7d844e43fd1bec4380bc3438389dea05
deno750 <deronjic.denis@gmail.com>
2020-04-16 15:49:15 +0200

    Lab2: the main tasks are finished. Need to clean up the code and other refiniements

--- src/Lab2/lab2.cpp
+++ src/Lab2/lab2.cpp
@@ -12,7 +12,7 @@ int main() {
     string path = "data/checkerboard_images/";
     dir = opendir(path.c_str());
     if (dir != NULL) {
-        cout << "Direcotry found!\n";
+        cout << "Directory found!\n";
     } else {
         cout << "Directory not found!\n";
     }
@@ -25,52 +25,96 @@ int main() {
     cv::Size chessSize(6, 5);
     
     std::vector<cv::Point3f> objc;
-    for (int y = 1; y <= chessSize.height; ++y) {
+    
+    //The origin (0,0,0) of the chessboard is located at the first square intersection in the top right.
+    for (int y = 0; y < chessSize.height; ++y) {
         
-        for (int x = 1; x <= chessSize.width; ++x) {
+        for (int x = 0; x < chessSize.width; ++x) {
+            //0.11 m for each square in chessboard
             objc.push_back(cv::Point3f(x * 0.11, y * 0.11, 0));
         }
     }
     
-    for (int i = 0; i < objc.size(); ++i) {
-        cout << objc[i] << " ";
-    }
-    cout << endl;
-    
+    std::vector<std::string> imageNames;
     while ((image = readdir(dir)) != NULL) {
-        if (strcmp(image->d_name, ".") == 0 || strcmp(image->d_name, "..") == 0) {
-            continue;
-        }
         
         string imagePath = path + image->d_name;
         mat = cv::imread(imagePath);
         if (mat.empty()) {
             continue;
         }
-        
         std::vector<cv::Point2f> corners;
         bool patternFound = cv::findChessboardCorners(mat, chessSize, corners); //This function works only with grey scale images. In this homework, images are already in grey.
         
         if (patternFound) {
+            imageNames.push_back(image->d_name);
             imagePoints.push_back(corners);
             objectPoints.push_back(objc);
-            
         }
         
     }
-    cv::calibrateCamera(objectPoints, imagePoints, mat.size(), cameraMatrix, distCoeffs, rvecs, tvecs);
+    
+    //Takes the size of the latest image because the images are all the same size.
+    double cameraError = cv::calibrateCamera(objectPoints, imagePoints, mat.size(), cameraMatrix, distCoeffs, rvecs, tvecs);
+    cout << "Intrinsic coeffs:\n " << cameraMatrix << endl;
+    cout << "Distortion coefficents:\n " << distCoeffs << endl;
+    cout << "Camera error: " << cameraError << endl;
+    
+    
+    double reprojectionError = 0;
+    std::vector<cv::Point2f> reprojectedPoints;
+    std::vector<double> errors;
+    int totalPoints = 0;
+    for (int i = 0; i < objectPoints.size(); ++i) {
+        cv::projectPoints(objectPoints[i], rvecs.row(i), tvecs.row(i), cameraMatrix, distCoeffs, reprojectedPoints);
+        double error = cv::norm(imagePoints[i], reprojectedPoints, cv::NORM_L2SQR);
+        errors.push_back(error);
+        reprojectionError += error;
+        totalPoints += objectPoints[i].size();
+    }
+    
+    double meanReprojectionError = sqrt(reprojectionError / totalPoints);
+    cout << "Reprojection error: " << meanReprojectionError << endl;
+    
+    double minError, maxError;
+    minError = errors.size();
+    maxError = minError;
+    int minIndex, maxIndex;
+    for (int i = 0; i < errors.size(); ++i) {
+        if (errors[i] < minError) {
+            minError = errors[i];
+            minIndex = i;
+        }
+        if (errors[i] > maxError) {
+            maxError = errors[i];
+            maxIndex = i;
+        }
+    }
+    
+    cout << "Image " << imageNames[minIndex] << " has got minimum error which is " << minError / objectPoints[minIndex].size() << endl;
+    cout << "Image " << imageNames[maxIndex] << " has got maximum error which is " << maxError / objectPoints[maxIndex].size() << endl;
+    
     cv::Mat R, map1, map2;
     cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, R, cameraMatrix, mat.size(), CV_32FC1, map1, map2);
+    
+    
+    
     cv::Mat src, dst;
-    cout << "map1" << endl;
-    cout << map1 << endl;
     src = cv::imread("data/test_image.png");
     if (src.empty()) {
         cout << "Image not found!\n";
     }
-    cv::remap(src, dst, map1, map2, cv::INTER_NEAREST);
+    cv::remap(src, dst, map1, map2, cv::INTER_LINEAR);
+    cv::resize(src, src, cv::Size(src.cols / 3, src.rows / 3));
+    cv::resize(dst, dst, cv::Size(dst.cols / 3, dst.rows / 3));
+    
+    
     cv::imshow("ORIGINAL", src);
     cv::imshow("REMAPPED", dst);
+    cv::moveWindow("REMAPPED", src.cols, 0);
+    cout << "\n=============================================" << endl;
+    cout << "                      END                      " << endl;
+     cout << "=============================================\n" << endl;
     cv::waitKey(0);
     return 0;
 }
